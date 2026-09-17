3d5d2c866dfe28aa977ccf44db0a007e3e5861cf
S. de Rooij <sebastiaan.de.rooij@woodwing.com>
2026-09-17 15:04:46 +0200

    Register PostgreSQL DQL functions in one place

--- tests/Integration/PostgresqlIntegrationTestCase.php
+++ tests/Integration/PostgresqlIntegrationTestCase.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 namespace Scienta\DoctrineJsonFunctions\Tests\Integration;
 
 use Doctrine\ORM\Configuration;
-use Scienta\DoctrineJsonFunctions\Query\AST\Functions\Postgresql as PgFunctions;
+use Scienta\DoctrineJsonFunctions\Tests\Query\PostgresqlTestCase;
 use Override;
 
 abstract class PostgresqlIntegrationTestCase extends IntegrationTestCase
@@ -42,18 +42,6 @@ abstract class PostgresqlIntegrationTestCase extends IntegrationTestCase
     #[Override]
     protected static function loadDqlFunctions(Configuration $config): void
     {
-        // Register all 11 PostgreSQL functions (the unit-test PostgresqlTestCase only
-        // registers 5; here we need them all for comprehensive integration testing).
-        $config->addCustomStringFunction(PgFunctions\JsonbContains::FUNCTION_NAME, PgFunctions\JsonbContains::class);
-        $config->addCustomStringFunction(PgFunctions\JsonbExists::FUNCTION_NAME, PgFunctions\JsonbExists::class);
-        $config->addCustomStringFunction(PgFunctions\JsonbExistsAll::FUNCTION_NAME, PgFunctions\JsonbExistsAll::class);
-        $config->addCustomStringFunction(PgFunctions\JsonbExistsAny::FUNCTION_NAME, PgFunctions\JsonbExistsAny::class);
-        $config->addCustomStringFunction(PgFunctions\JsonbInsert::FUNCTION_NAME, PgFunctions\JsonbInsert::class);
-        $config->addCustomStringFunction(PgFunctions\JsonbIsContained::FUNCTION_NAME, PgFunctions\JsonbIsContained::class);
-        $config->addCustomStringFunction(PgFunctions\JsonExtractPath::FUNCTION_NAME, PgFunctions\JsonExtractPath::class);
-        $config->addCustomStringFunction(PgFunctions\JsonGet::FUNCTION_NAME, PgFunctions\JsonGet::class);
-        $config->addCustomStringFunction(PgFunctions\JsonGetPath::FUNCTION_NAME, PgFunctions\JsonGetPath::class);
-        $config->addCustomStringFunction(PgFunctions\JsonGetPathText::FUNCTION_NAME, PgFunctions\JsonGetPathText::class);
-        $config->addCustomStringFunction(PgFunctions\JsonGetText::FUNCTION_NAME, PgFunctions\JsonGetText::class);
+        PostgresqlTestCase::loadDqlFunctions($config);
     }
 }
--- tests/Query/PostgresqlTestCase.php
+++ tests/Query/PostgresqlTestCase.php
@@ -30,9 +30,15 @@ abstract class PostgresqlTestCase extends DbTestCase
     public static function loadDqlFunctions(Configuration $configuration)
     {
         $configuration->addCustomStringFunction(DqlFunctions\JsonbContains::FUNCTION_NAME, DqlFunctions\JsonbContains::class);
-        $configuration->addCustomStringFunction(DqlFunctions\JsonbInsert::FUNCTION_NAME, DqlFunctions\JsonbInsert::class);
-        $configuration->addCustomStringFunction(DqlFunctions\JsonGetText::FUNCTION_NAME, DqlFunctions\JsonGetText::class);
-        $configuration->addCustomStringFunction(DqlFunctions\JsonbExistsAny::FUNCTION_NAME, DqlFunctions\JsonbExistsAny::class);
+        $configuration->addCustomStringFunction(DqlFunctions\JsonbExists::FUNCTION_NAME, DqlFunctions\JsonbExists::class);
         $configuration->addCustomStringFunction(DqlFunctions\JsonbExistsAll::FUNCTION_NAME, DqlFunctions\JsonbExistsAll::class);
+        $configuration->addCustomStringFunction(DqlFunctions\JsonbExistsAny::FUNCTION_NAME, DqlFunctions\JsonbExistsAny::class);
+        $configuration->addCustomStringFunction(DqlFunctions\JsonbInsert::FUNCTION_NAME, DqlFunctions\JsonbInsert::class);
+        $configuration->addCustomStringFunction(DqlFunctions\JsonbIsContained::FUNCTION_NAME, DqlFunctions\JsonbIsContained::class);
+        $configuration->addCustomStringFunction(DqlFunctions\JsonExtractPath::FUNCTION_NAME, DqlFunctions\JsonExtractPath::class);
+        $configuration->addCustomStringFunction(DqlFunctions\JsonGet::FUNCTION_NAME, DqlFunctions\JsonGet::class);
+        $configuration->addCustomStringFunction(DqlFunctions\JsonGetPath::FUNCTION_NAME, DqlFunctions\JsonGetPath::class);
+        $configuration->addCustomStringFunction(DqlFunctions\JsonGetPathText::FUNCTION_NAME, DqlFunctions\JsonGetPathText::class);
+        $configuration->addCustomStringFunction(DqlFunctions\JsonGetText::FUNCTION_NAME, DqlFunctions\JsonGetText::class);
     }
 }
